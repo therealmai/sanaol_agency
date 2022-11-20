920b152e1bc5e07e1d2469c3a50245a7bfb64b44
Ivan Woogue <ivanwoogue2@gmail.com>
2022-11-20 08:53:49 +0800

    Done with get, patch endpoints

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Service;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ServiceController extends Controller
 {
-    private $TB_NAME = 'service';
+    private $TB_NAME = 'services';
+    private $MSG_ERR_ID_NOT_FOUND = 'ID did not match any of the service found in database.';
+    private $MSG_ERR_ADDITIONAL_PROPS = 'Model does not contain properties found in request';
+    private $MSG_SUC_ID_FOUND = 'Service found.';
+    private $MSG_SUC_UPDATE = 'Service successfully updated.';
 
     /**
      * Display a listing of the resource.
@@ -17,7 +23,7 @@ class ServiceController extends Controller
     {
         $services = Service::all();
 
-        return response($services, 200);
+        return response($services, 200, ['application/json']);
     }
 
     /**
@@ -42,25 +48,26 @@ class ServiceController extends Controller
     }
 
     /**
-     * Display the specified resource.
+     * Get one service by id
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param [type] $id
+     * @return void
      */
     public function show($id)
     {
-        //
-    }
+        $service = Service::find($id);
+        $msg = '';
+        $status = '';
+        
+        if($service == NULL) {
+            $msg = $this->MSG_ERR_ID_NOT_FOUND;
+            $status = 404;
+        } else {
+            $msg = $this->MSG_SUC_ID_FOUND;
+            $status = 200;
+        }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
+        return response($this->generateRes($service, $status, $msg), 200, ['application/json']);
     }
 
     /**
@@ -72,7 +79,41 @@ class ServiceController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $service = Service::find($id);
+        $input = $request->input();
+        $msg = '';
+        $status = '';
+        $err = false;
+
+        if($service == NULL) {
+            $msg = $this->MSG_ERR_ID_NOT_FOUND;
+            $status = 404;
+        } 
+
+        if($service != NULL) {
+            foreach($input as $key => $val) {
+                if(isset($service[$key])) {
+                    $service[$key] = $val;
+                } 
+                else {
+                    $err = true;
+                    break;
+                }
+            }  
+        }
+
+        if($err) {
+            $msg = $this->MSG_ERR_ADDITIONAL_PROPS;
+            $status = 500;
+            $service = null;
+        } else {
+            $msg = $this->MSG_SUC_UPDATE;
+            $status = 204;
+            $service->updated_at = Carbon::now($this->TZ_OFFSET)->toDateTimeString();
+            $service->save();
+        }
+
+        return response($this->generateRes($service, $status, $msg), 200, ['application/json']);
     }
 
     /**
@@ -84,5 +125,14 @@ class ServiceController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    public function generateRes($data, $status, $msg) {
+        $res = [
+            'data' => $data,
+            'status' => $status,
+            'msg' => $msg
+        ];
+        return $res;
     }
 }
