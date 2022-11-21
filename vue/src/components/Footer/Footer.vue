d0791019264a5ee5c6af988352a1da3f91178dbe
Hipopothanos <iympassible@gmail.com>
2022-11-21 12:30:43 +0800

    style: Update Footer CS-09

--- vue/src/components/Footer/Footer.vue
+++ vue/src/components/Footer/Footer.vue
@@ -1,6 +1,108 @@
 <template>
   <div class="bg-primary flex inset-x-0 bottom-0 justify-between items-center space-x-4 h-[77px] min-h-[287.85px] w-full ">
       <!-- Footer Contents -->
+      <footer class="text-center lg:text-left footer-bg text-white">
+  <div class="mx-6 py-10 text-center md:text-left">
+    <div class="grid grid-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+      <div class="">
+        <h6 class= "
+            
+            font-semibold
+            mb-4
+            flex
+            items-center
+            justify-center
+            md:justify-start
+            text-white
+          ">
+          <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="cubes"
+            class="w-4 mr-3" role="img" xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512">
+            <path fill="currentColor"
+              d="M488.6 250.2L392 214V105.5c0-15-9.3-28.4-23.4-33.7l-100-37.5c-8.1-3.1-17.1-3.1-25.3 0l-100 37.5c-14.1 5.3-23.4 18.7-23.4 33.7V214l-96.6 36.2C9.3 255.5 0 268.9 0 283.9V394c0 13.6 7.7 26.1 19.9 32.2l100 50c10.1 5.1 22.1 5.1 32.2 0l103.9-52 103.9 52c10.1 5.1 22.1 5.1 32.2 0l100-50c12.2-6.1 19.9-18.6 19.9-32.2V283.9c0-15-9.3-28.4-23.4-33.7zM358 214.8l-85 31.9v-68.2l85-37v73.3zM154 104.1l102-38.2 102 38.2v.6l-102 41.4-102-41.4v-.6zm84 291.1l-85 42.5v-79.1l85-38.8v75.4zm0-112l-102 41.4-102-41.4v-.6l102-38.2 102 38.2v.6zm240 112l-85 42.5v-79.1l85-38.8v75.4zm0-112l-102 41.4-102-41.4v-.6l102-38.2 102 38.2v.6z">
+            </path>
+          </svg>
+          Contact Us
+        </h6>
+        <p class="mb-2">
+          <a href="#!" class="text-white">(032) 255 0111</a>
+        </p>
+        <p class="mb-2">
+          <a href="#!" class="text-white">info@system.com</a>
+        </p>
+      </div>
+      <div class="">
+        <h6 class=" font-semibold mb-4 flex justify-center md:justify-start ">
+          Operating Hours
+        </h6>
+        <p class="mb-2">
+          <a href="#!" class="text-white">Monday to Friday</a>
+        </p>
+        <p class="mb-2">
+          <a href="#!" class="text-white">8:00 AM to 6:00 PM</a>
+        </p>
+      </div>
+      <div class="">
+        <h6 class=" font-semibold mb-4 flex justify-center md:justify-start">
+          Address
+        </h6>
+        <p class="mb-2">
+          <a href="#!" class="text-white">J Block Bldg, Brgy. 3 571, The, 2 Apitong St, Cebu City, Cebu
+        </a>
+        </p>    
+      </div>
+      <div class="">
+        <h6 class=" font-semibold mb-4 flex justify-center md:justify-start">
+          Social Media
+        </h6>
+        <div class="flex justify-start">
+                <a href="#!" class="mr-4 text-white">
+                <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="facebook-f"
+                class="w-7" role="img" xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512">
+                <path fill="currentColor"
+                    d="M400 32H48A48 48 0 0 0 0 80v352a48 48 0 0 0 48 48h137.25V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.27c-30.81 0-40.42 19.12-40.42 38.73V256h68.78l-11 71.69h-57.78V480H400a48 48 0 0 0 48-48V80a48 48 0 0 0-48-48z">
+                </path>
+                </svg>
+           
+            </a>
+            <a href="#!" class="mr-4 text-white">
+                <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="instagram"
+                class="w-6" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                <path fill="currentColor"
+                    d="M224,202.66A53.34,53.34,0,1,0,277.36,256,53.38,53.38,0,0,0,224,202.66Zm124.71-41a54,54,0,0,0-30.41-30.41c-21-8.29-71-6.43-94.3-6.43s-73.25-1.93-94.31,6.43a54,54,0,0,0-30.41,30.41c-8.28,21-6.43,71.05-6.43,94.33S91,329.26,99.32,350.33a54,54,0,0,0,30.41,30.41c21,8.29,71,6.43,94.31,6.43s73.24,1.93,94.3-6.43a54,54,0,0,0,30.41-30.41c8.35-21,6.43-71.05,6.43-94.33S357.1,182.74,348.75,161.67ZM224,338a82,82,0,1,1,82-82A81.9,81.9,0,0,1,224,338Zm85.38-148.3a19.14,19.14,0,1,1,19.13-19.14A19.1,19.1,0,0,1,309.42,189.74ZM400,32H48A48,48,0,0,0,0,80V432a48,48,0,0,0,48,48H400a48,48,0,0,0,48-48V80A48,48,0,0,0,400,32ZM382.88,322c-1.29,25.63-7.14,48.34-25.85,67s-41.4,24.63-67,25.85c-26.41,1.49-105.59,1.49-132,0-25.63-1.29-48.26-7.15-67-25.85s-24.63-41.42-25.85-67c-1.49-26.42-1.49-105.61,0-132,1.29-25.63,7.07-48.34,25.85-67s41.47-24.56,67-25.78c26.41-1.49,105.59-1.49,132,0,25.63,1.29,48.33,7.15,67,25.85s24.63,41.42,25.85,67.05C384.37,216.44,384.37,295.56,382.88,322Z">
+                </path>
+                </svg>
+               
+            </a>
+            <a href="#!" class="mr-4 text-white">
+                <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="twitter"
+                class="w-7" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <path fill="currentColor"
+                    d="M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zm-48.9 158.8c.2 2.8.2 5.7.2 8.5 0 86.7-66 186.6-186.6 186.6-37.2 0-71.7-10.8-100.7-29.4 5.3.6 10.4.8 15.8.8 30.7 0 58.9-10.4 81.4-28-28.8-.6-53-19.5-61.3-45.5 10.1 1.5 19.2 1.5 29.6-1.2-30-6.1-52.5-32.5-52.5-64.4v-.8c8.7 4.9 18.9 7.9 29.6 8.3a65.447 65.447 0 0 1-29.2-54.6c0-12.2 3.2-23.4 8.9-33.1 32.3 39.8 80.8 65.8 135.2 68.6-9.3-44.5 24-80.6 64-80.6 18.9 0 35.9 7.9 47.9 20.7 14.8-2.8 29-8.3 41.6-15.8-4.9 15.2-15.2 28-28.8 36.1 13.2-1.4 26-5.1 37.8-10.2-8.9 13.1-20.1 24.7-32.9 34z">
+                </path>
+                </svg>
+              
+            </a>
+            <a href="#!" class="mr-4 text-white">
+                <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="youtube"
+                class="w-7" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <path fill="currentColor"
+                    d="M186.8 202.1l95.2 54.1-95.2 54.1V202.1zM448 80v352c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V80c0-26.5 21.5-48 48-48h352c26.5 0 48 21.5 48 48zm-42 176.3s0-59.6-7.6-88.2c-4.2-15.8-16.5-28.2-32.2-32.4C337.9 128 224 128 224 128s-113.9 0-142.2 7.7c-15.7 4.2-28 16.6-32.2 32.4-7.6 28.5-7.6 88.2-7.6 88.2s0 59.6 7.6 88.2c4.2 15.8 16.5 27.7 32.2 31.9C110.1 384 224 384 224 384s113.9 0 142.2-7.7c15.7-4.2 28-16.1 32.2-31.9 7.6-28.5 7.6-88.1 7.6-88.1z">
+                </path>
+                </svg>
+
+        
+            </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="text-center p-6 footer-background-color text-white">
+    <span>© 2022 Sanaol Agency. All Rights Reserved.
+</span>
+    </div>
+</footer>
       
 
   </div>
