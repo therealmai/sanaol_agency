7d07e1a45f4db004496843c180bcac958b2c5f1c
Hipopothanos <iympassible@gmail.com>
2022-11-22 19:26:40 +0800

    style: Talent_User Updated Template

--- vue/src/pages/Talent_User.vue
+++ vue/src/pages/Talent_User.vue
@@ -2,12 +2,13 @@
 <body>
     <div style="padding-right: 90%;  background-color: #F8F8FF; padding-bottom: 150px;padding-top: 30px;">
     <h1 style="padding-left: 50px;font-weight: 500; font-size: 32px; color: #525252;">Profile</h1>
-        <div style= "position: absolute; left: 20%; top: 40%; color: #7367F0">
-            <h5 style="position: absolute; left: 170px;top: 20%; width: 200px; font-weight: bold; ">Sebastian Villanueva</h5>
+        <div class= "name-email">
+            <h5 class="user-name">Sebastian Villanueva</h5>
             <p style="position: absolute; left: 170px;top: 50px;font-size: small;">@JonathanVillanueva</p>
             <img style="width: 158px; height: 158px; border-radius: 100%" src="https://pixy.org/src/477/4774988.jpg" alt="alternatetext">
-            <p style="position: absolute; left: 170px;top: 55%; width: 800px;">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit aut suscipit eum minus, omnis adipisci dicta molestiae corrupti laudantium exercitationem mollitia ipsa illo cupiditate sunt accusamus maxime nostrum aliquid. Praesentium?</p>
-            
+            <div id= "par-pos">
+                <p style=" width: 800px;">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit aut suscipit eum minus, omnis adipisci dicta molestiae corrupti laudantium exercitationem mollitia ipsa illo cupiditate sunt accusamus maxime nostrum aliquid. Praesentium?</p>
+            </div>
         
         </div>
 
