e0dd8c9a53a3e3c1831e226bd2d3ae3005678f70
Christian <christianstewart5111@gmail.com>
2022-11-23 22:00:19 +0800

    Refactor: Updated Login & Membership pages | CS-13

--- vue/src/pages/Login.vue
+++ vue/src/pages/Login.vue
@@ -1,22 +1,42 @@
 <template>
 <body>
 
-    <div class="flex items-center justify-center container">
+    <div class="flex items-center justify-center container h-fit">
     <!-- insert log in template here. -->
-      <div class="md:p-12 md:mx-6">
+      <div class="md:p-4 md:mx-6 m-8">
         <div class="text-center">
           <img
             class="mx-auto w-48"
             src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/lotus.webp"
             alt="logo"
           />
-          <h4 class="text-4xl font-semibold mt-1 mb-12 pb-1">Sanaol Agency</h4>
+          <h4 class="text-2xl font-semibold mt-1 mb-6 pb-1">Sanaol Agency</h4>
         </div>
 
-        <form>
+        <form id="login" method="post" @submit="login">
+
+          <!-- Error Modal -->
+          <div v-if="errorModal" id="popup-modal" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 p-4 md:inset-0 h-modal md:h-full">
+                <div class="relative w-full max-w-md h-full md:h-auto">
+                    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                        <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-toggle="popup-modal">
+                            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                            <span class="sr-only">Close modal</span>
+                        </button>
+                        <div class="p-6 text-center">
+                            <svg aria-hidden="true" class="mx-auto mb-4 w-14 h-14 text-gray-400 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                            <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this product?</h3>
+                            <button data-modal-toggle="popup-modal" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
+                                Retry
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
           <!-- email -->
           <div class="relative z-0 mb-6 w-full group">
-            <input type="email" name="email" id="email" v-model="email" placeholder=" " required
+            <input type="email" name="email" id="email" v-model="user.email" placeholder=" " required autocomplete="email"
               class="block py-2 px-3 w-full text-base text-gray-700 bg-transparent border border-solid border-gray-300
               rounded dark:text-gray-700 dark:border-gray-300 dark:focus:border-indigo-500 focus:outline-none focus:ring-0
               focus:border--indigo-500 peer"/>
@@ -29,7 +49,7 @@
           
           <!-- password -->
           <div class="relative z-0 mb-0 w-full group">
-            <input type="password" name="password" id="password" v-model="password"  placeholder=" " required
+            <input type="password" name="password" id="password" v-model="user.password"  placeholder=" " required
               class="block py-2 px-3 w-full text-base text-gray-700 bg-transparent border border-solid border-gray-300
               rounded dark:text-gray-700 dark:border-gray-300 dark:focus:border-indigo-500 focus:outline-none focus:ring-0
               focus:border--indigo-500 peer"/>
@@ -48,45 +68,30 @@
           </div>
 
           <!-- Login Button -->
-          <div class="text-center pt-1 mb-12 pb-1">
+          <div class="text-center pt-1 mb-4 pb-1">
             <button
               class="inline-block px-6 py-4 text-white bg-indigo-500 font-medium text-base leading-tight
               uppercase rounded shadow-md focus:shadow-lg
               hover:bg-indigo-50 hover:shadow-lg hover:text-indigo-500
               focus:outline-none focus:ring-0 active:shadow-lg transition duration-300 ease-in-out w-full mb-3"
-              type="button"
+              type="submit"
               data-mdb-ripple="true"
               data-mdb-ripple-color="light"
-              @click="isUser = true"
               data-model-toggle="popup-modal"
             >
               Log in
             </button>
 
-            <!-- <div id="popup-modal" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 p-4 md:inset-0 h-modal md:h-full">
-                <div class="relative w-full max-w-md h-full md:h-auto">
-                    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                        <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-toggle="popup-modal">
-                            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                            <span class="sr-only">Close modal</span>
-                        </button>
-                        <div class="p-6 text-center">
-                            <svg aria-hidden="true" class="mx-auto mb-4 w-14 h-14 text-gray-400 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                            <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this product?</h3>
-                            <button data-modal-toggle="popup-modal" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
-                                Yes, I'm sure
-                            </button>
-                            <button data-modal-toggle="popup-modal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">No, cancel</button>
-                        </div>
-                    </div>
-                </div>
-            </div> -->
+            
           </div>
 
           <!-- Membership Button -->
-          <div class="flex items-center justify-between pb-6">
-            <p class="mb-0 mr-2">Don't have an account?</p>
-            <a href="#!"
+          <div class="flex items-center justify-between pb-3">
+            <p class="mb-0 mr-2">Don't have an account?
+            {{ '' }}
+            <!-- <router-link :to="{name: 'Membership'}" class="underline text-indigo-500 hover:text-indigo-400">Apply as a Talent here.</router-link> -->
+            </p>
+            <a href="./Membership"
             class="underline"
             style="color:#7367f0">Apply as a Talent here.</a> 
           </div>
@@ -94,25 +99,41 @@
         </form>
       </div>
     </div>
-    
-
 
     <!-- image -->
-    <div class="w-full object-cover h-72 lg:w-full md:h-screen bg-cover bg-center" style="background-image:url(https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80);"></div>
+    <div class="object-cover h-auto lg:w-screen md:h-auto bg-cover bg-center" style="background-image:url(https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80);"></div>
 </body>
 </template>
 
-<script>
+<script setup>
 import Navbar from '../components/Navigation/Navbar.vue';
 import Modal from '../components/Modal/Modal.vue';
+import store from "../store";
+import {useRouter} from "vue-router";
+import Footer from '../components/Footer/Footer.vue';
 
-export default{
-  components: {
-    Navbar,
-    Modal,
+const router = useRouter();
 
-  },
+const user = {
+  email: '',
+  password: '',
 }
+
+function login(ev) {
+  ev.preventD,
+Footerefault();
+
+  store.dispatch('login', user)
+    .then(() => {
+      router.push({
+        name:'Hero'
+      })
+    })
+    .catch(err => {
+      errorModal = Modal
+    })
+}
+
 </script>
 
 <style>
--- vue/src/pages/Membership.vue
+++ vue/src/pages/Membership.vue
@@ -8,7 +8,7 @@
         <div class="md:p-12 md:mx-6 align-middle flex items-center justify-center">
           
           <!-- Form -->
-          <form>
+          <form id="register" @submit="register" method="post">
             <!-- Title -->
             <div class="mb-10">
               <h1 class="text-4xl font-bold">Become a Rising Star</h1>
@@ -17,7 +17,7 @@
             <!-- Form Group -->
             <div class="grid md:grid-cols-2 md:gap-6">
               <div class="relative z-0 mb-6 w-full group">
-                <input type="text" name="fname" id="fname" v-model="text" placeholder=" " required
+                <input type="text" name="fname" id="fname" v-model="user.first_name" placeholder=" " required autocomplete="name"
                   class="block py-2 px-3 w-full text-base text-gray-700 bg-transparent border border-solid border-gray-300
                   rounded dark:text-gray-700 dark:border-gray-300 dark:focus:border-indigo-500 focus:outline-none focus:ring-0
                   focus:border--indigo-500 peer"/>
@@ -28,7 +28,7 @@
                   peer-focus:scale-75 peer-focus:-translate-y-8">First Name</label>
               </div>
               <div class="relative z-0 mb-6 w-full group">
-                <input type="text" name="lname" id="lname" v-model="text" placeholder=" " required
+                <input type="text" name="lname" id="lname" v-model="user.last_name" placeholder=" " required autocomplete="name"
                   class="block py-2 px-3 w-full text-base text-gray-700 bg-transparent border border-solid border-gray-300
                   rounded dark:text-gray-700 dark:border-gray-300 dark:focus:border-indigo-500 focus:outline-none focus:ring-0
                   focus:border--indigo-500 peer"/>
@@ -42,7 +42,7 @@
 
             <!-- Instagram Handle -->
             <div class="relative z-0 mb-6 w-full group">
-              <input type="text" name="handle" id="handle" v-model="text" placeholder=" " required
+              <input type="text" name="handle" id="handle" v-model="user.instagram_handle" placeholder=" " required
                 class="block py-2 px-3 w-full text-base text-gray-700 bg-transparent border border-solid border-gray-300
                 rounded dark:text-gray-700 dark:border-gray-300 dark:focus:border-indigo-500 focus:outline-none focus:ring-0
                 focus:border--indigo-500 peer"/>
@@ -55,7 +55,7 @@
 
             <!-- Email -->
             <div class="relative z-0 mb-6 w-full group">
-              <input type="email" name="email" id="email" v-model="email" placeholder=" " required
+              <input type="email" name="email" id="email" v-model="user.email" placeholder=" " required autocomplete="email"
                 class="block py-2 px-3 w-full text-base text-gray-700 bg-transparent border border-solid border-gray-300
                 rounded dark:text-gray-700 dark:border-gray-300 dark:focus:border-indigo-500 focus:outline-none focus:ring-0
                 focus:border--indigo-500 peer"/>
@@ -68,7 +68,7 @@
             
             <!-- Password -->
             <div class="relative z-0 mb-6 w-full group">
-              <input type="password" name="password" id="password" v-model="password"  placeholder=" " required
+              <input type="password" name="password" id="password" v-model="user.password"  placeholder=" " required
                 class="block py-2 px-3 w-full text-base text-gray-700 bg-transparent border border-solid border-gray-300
                 rounded dark:text-gray-700 dark:border-gray-300 dark:focus:border-indigo-500 focus:outline-none focus:ring-0
                 focus:border--indigo-500 peer"/>
@@ -81,7 +81,7 @@
 
             <!-- Confirm Password -->
             <div class="relative z-0 mb-6 w-full group">
-              <input type="password" name="confirm_password" id="confirm_password" v-model="password"  placeholder=" " required
+              <input type="password" name="confirm_password" id="confirm_password" v-model="user.password_confirmation"  placeholder=" " required
                 class="block py-2 px-3 w-full text-base text-gray-700 bg-transparent border border-solid border-gray-300
                 rounded dark:text-gray-700 dark:border-gray-300 dark:focus:border-indigo-500 focus:outline-none focus:ring-0
                 focus:border--indigo-500 peer"/>
@@ -94,7 +94,7 @@
             
             <!-- Why Join Sanaol Agency -->
             <div class="relative z-0 mb-6 w-full group">
-              <textarea name="reason" id="reason" v-model="text"  placeholder=" " required rows="5"
+              <textarea name="reason" id="reason" placeholder=" " required rows="5"
                 class="block py-2 px-3 w-full text-base text-gray-700 bg-transparent border border-solid border-gray-300
                 rounded dark:text-gray-700 dark:border-gray-300 dark:focus:border-indigo-500 focus:outline-none focus:ring-0
                 focus:border--indigo-500 peer"/>
@@ -107,7 +107,7 @@
 
             <!-- Submit Button -->
             <button type="submit" class="
-              w-full px-6 py-3
+              w-full px-6 py-3 mb-6
               bg-indigo-500 text-white font-medium text-base
               leading-tight uppercase rounded shadow-md
               hover:bg-indigo-50 hover:shadow-lg hover:text-indigo-500
@@ -115,19 +115,53 @@
               active:bg-indigo-800 active:shadow-lg
               transition duration-300
               ease-in-out">APPLY AS A MEMBER</button>
+
+              <div class="items-center flex pb-11">
+                <p class="mb-0 mr-2">Already have an account?
+                {{ '' }}
+                <!-- <router-link :to="{name: 'Login'}" class="underline text-indigo-500 hover:text-indigo-400">Login here.</router-link> -->
+                </p>
+                <a href="./Login"
+                class="underline"
+                style="color:#7367f0">Login here.</a> 
+              </div>
           </form>
+
+          
       </div>
     </div>
 </body>
 </template>
 
 
-<script>
+<script setup>
+import { useRouter } from 'vue-router';
 import Navbar from '../components/Navigation/Navbar.vue';
-export default{
-  components: {
-    Navbar,
-  },
+import store from "../store";
+
+const router = useRouter();
+
+const user = {
+  first_name: '',
+  last_name: '',
+  instagram_handle: '',
+  email: '',
+  password: '',
+  password_confirmation: '',
+  reason: '',
+}
+
+function register(ev) {
+  ev.prevenDefault();
+  store.dispatch('register', user)
+       .then(() => {
+          router.push({
+            name: 'Hero'
+          })
+       })
+       .catch(err => {
+          errorModal = Modal
+       })
 }
 </script>
 
