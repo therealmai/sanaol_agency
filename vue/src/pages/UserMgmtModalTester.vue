b679723429b3ff45f744558a7519be64c856ba48
w3barsi <darcybalaga@gmail.com>
2022-11-24 14:33:19 +0800

    feat: cleaned up methods using switch case

--- vue/src/pages/UserMgmtModalTester.vue
+++ vue/src/pages/UserMgmtModalTester.vue
@@ -1,13 +1,53 @@
 <script>
 import EditUserModal from "../components/Modal/EditUserModal.vue";
+import ViewApplicationModal from "../components/Modal/ViewApplicationModal.vue"
+import Sample from "../pages/Sample.vue";
 
 export default {
   components: {
     EditUserModal,
+    ViewApplicationModal
+  },
+  data() {
+    return {
+      isModalVisible: false,
+      isApplicationModalVisible: false,
+    };
+  },
+  methods: {
+    showModal(name) {
+      switch (name) {
+        case "user":
+          this.isModalVisible = true;
+          break;
+        case "application":
+          this.isApplicationModalVisible = true;
+          break;
+      }
+    },
+    closeModal(name) {
+      switch (name) {
+        case "user":
+          this.isModalVisible = false;
+          break;
+        case "application":
+          this.isApplicationModalVisible = false;
+          break;
+      }
+    },
   },
 };
 </script>
 
 <template>
-  <edit-user-modal/>
+  <div class="flex flex-col justify-center w-full items-center pt-5">
+    <button class="btn-primary" @click="showModal('user')">
+      OPEN EDIT USER MODAL
+    </button>
+    <button class="btn-primary" @click="showModal('application')">
+      View Application Modal
+    </button>
+  </div>
+  <edit-user-modal v-show="isModalVisible" @close="closeModal('user')" />
+  <view-application-modal v-show="isApplicationModalVisible" @close="closeModal('application')" />
 </template>
