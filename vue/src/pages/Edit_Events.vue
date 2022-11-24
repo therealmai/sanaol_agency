bb24364aab6c39582a5c5cf3e6b3c88ef2202e8b
Hipopothanos <iympassible@gmail.com>
2022-11-24 19:27:26 +0800

    style: Edit Events formation CS-36

--- vue/src/pages/Edit_Events.vue
+++ vue/src/pages/Edit_Events.vue
@@ -1,11 +1,23 @@
 <template>
-    <p1> Edit Events</p1>
-    <modal></modal>
+<body style="margin-top: 20px;">
+    <primary-button  text="cancel" height="25" width="100" color="White" fontSize="12" @click="$router.push('Events')"></primary-button>
+    <div style="margin-left: 300px; margin-top: 20px;">
+        <p1 style="font-weight: bold; font-size: 30px;">Edit Events</p1>
+        <div style="margin-left: 30px;">
+            <img src="https://i.ytimg.com/vi/zPCZWn_iWb0/maxresdefault.jpg" alt="cute cat" style="width: 388px; height: 222px">
+            <p1>Input</p1><br>
+            <p1>Input</p1><br>
+            <p1>Input</p1>
+        </div>
+    </div>
+</body>
 </template>
 
 <script>
-import Modal from '../components/Modal/Modal.vue'
+import PrimaryButton from '../components/Buttons/PrimaryButton.vue'
 export default {
-  components: { Modal },
+  components: { 
+    PrimaryButton
+     },
 }
 </script>
