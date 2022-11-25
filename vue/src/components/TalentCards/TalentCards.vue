9704c3484d3669b8848d6c3d46e0b841a0ddc429
Marc Nathaniel Valeros <marcnathanielvaleros@gmail.com>
2022-11-25 20:27:02 +0800

    Style: Fix Styling and Layout of Cards

--- vue/src/components/TalentCards/TalentCards.vue
+++ vue/src/components/TalentCards/TalentCards.vue
@@ -1,15 +1,16 @@
 <template>
-   <div class="max-w-sm bg-white border  rounded-2xl shadow-md" v-bind:id="id">
+   <div class="bg-white border rounded-2xl shadow-sm w-[277px]" v-bind:id="id">
         <a href="#">
             <img class="rounded-t-2xl " src="https://images.unsplash.com/photo-1470509037663-253afd7f0f51?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NjA5MzgxMzk&ixlib=rb-1.2.1&q=80" alt="" />
         </a>
+        
         <div class="flex items-center">
             <img class="w-11 h-11 rounded-full ml-4" src="https://images.unsplash.com/photo-1470509037663-253afd7f0f51?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NjA5MzgxMzk&ixlib=rb-1.2.1&q=80" alt="Neil image">
             <div class="p-3">
             <a href="#">
                 <p class="text-xl font-bold">Juan Dela Cruz</p>
             </a>
-            <p class="font-semibold text-black">@juandelacruz</p>
+            <p class="font-semibold text-secondary">@juandelacruz</p>
             </div>
         </div>
     </div> 
--- vue/src/pages/Talent_List.vue
+++ vue/src/pages/Talent_List.vue
@@ -1,59 +1,55 @@
 <template>
-<body>
-  <div class="pt-[96px] pl-[217.5px] pr-[2175px] mb-[18px]">
-    <h1 class="text-3xl font-bold text-gray-600">Our Talents</h1>
-    <div class="grid grid-cols-4 gap-16 justify-center pt-9">
-
-      <!-- Talent Card -->
-      <li :key="item" v-for="item in 10">
-        <TalentCard :id='item'/>
-      </li>
-
-    </div>
-    <!-- Pagination -->
-    <div class="flex justify-center pt-10">
-      <nav aria-label="Page navigation">
-        <ul class="flex list-style-none">
-          <li class="page-item"><a
-              class="page-link relative block py-1.5 px-3 rounded-full bg-transparent outline-none transition-all duration-300 text-gray-500 hover:text-gray-800 hover:bg-violet-100 focus:shadow-none"
-              href="#" aria-label="Previous">
-              <span aria-hidden="true">&lsaquo;</span>
-            </a></li>
-          <li class="page-item"><a
-              class="page-link relative block py-1.5 px-3 rounded-full bg-transparent outline-none transition-all duration-300 text-gray-500 hover:text-gray-800 hover:bg-violet-100 focus:shadow-none"
-              href="#">1</a></li>
-          <li class="page-item"><a
-              class="page-link relative block py-1.5 px-3 rounded-full bg-transparent outline-none transition-all duration-300 text-gray-500 hover:text-gray-800 hover:bg-violet-100 focus:shadow-none"
-              href="#">2</a></li>
-          <li class="page-item"><a
-              class="page-link relative block py-1.5 px-3 rounded-full bg-transparent outline-none transition-all duration-300 text-gray-500 hover:text-gray-800 hover:bg-violet-100 focus:shadow-none"
-              href="#">3</a></li>
-          <span class="rounded-md px-4 py-2 text-gray-500">...</span>
-          <li class="page-item"><a
-              class="page-link relative block py-1.5 px-3 rounded-full bg-transparent outline-none transition-all duration-300 text-gray-500 hover:text-gray-800 hover:bg-violet-100 focus:shadow-none"
-              href="#">11</a></li>
-          <li class="page-item"><a
-              class="page-link relative block py-1.5 px-3 rounded-full bg-transparent outline-none transition-all duration-300 text-gray-500 hover:text-gray-800 hover:bg-violet-100 focus:shadow-none"
-              href="#">12</a></li>
-          <li class="page-item"><a
-              class="page-link relative block py-1.5 px-3 rounded-full bg-transparent outline-none transition-all duration-300 text-gray-500 hover:text-gray-800 hover:bg-violet-100 focus:shadow-none"
-              href="#">13</a></li>
-          <li class="page-item"><a
-              class="page-link relative block py-1.5 px-3 rounded-full bg-transparent outline-none transition-all duration-300 text-gray-500 hover:text-gray-800 hover:bg-violet-100 focus:shadow-none"
-              href="#" aria-label="Next">
-              <span aria-hidden="true">&rsaquo;</span>
-            </a></li>
+    <div class=" flex flex-col justify-center items-center w-full">
+      <h1 class="text-3xl font-bold text-gray-600 mt-8 mr-[1070px]">Our Talents</h1>
+      <div class="grid grid-cols-4 gap-12 mt-10">
+        <!-- Talent Card -->
+        <ul :key="item" v-for="item in 10">
+          <TalentCard :id='item'/>
         </ul>
-      </nav>
+      </div>
+      <!-- Pagination -->
+      <div class="flex justify-center pt-10 mb-10">
+        <nav aria-label="Page navigation">
+          <ul class="flex list-style-none">
+            <li class="page-item"><a
+                class="page-link relative block py-1.5 px-3 rounded-full bg-transparent outline-none transition-all duration-300 text-gray-500 hover:text-gray-800 hover:bg-violet-100 focus:shadow-none"
+                href="#" aria-label="Previous">
+                <span aria-hidden="true">&lsaquo;</span>
+              </a></li>
+            <li class="page-item"><a
+                class="page-link relative block py-1.5 px-3 rounded-full bg-transparent outline-none transition-all duration-300 text-gray-500 hover:text-gray-800 hover:bg-violet-100 focus:shadow-none"
+                href="#">1</a></li>
+            <li class="page-item"><a
+                class="page-link relative block py-1.5 px-3 rounded-full bg-transparent outline-none transition-all duration-300 text-gray-500 hover:text-gray-800 hover:bg-violet-100 focus:shadow-none"
+                href="#">2</a></li>
+            <li class="page-item"><a
+                class="page-link relative block py-1.5 px-3 rounded-full bg-transparent outline-none transition-all duration-300 text-gray-500 hover:text-gray-800 hover:bg-violet-100 focus:shadow-none"
+                href="#">3</a></li>
+            <span class="rounded-md px-4 py-2 text-gray-500">...</span>
+            <li class="page-item"><a
+                class="page-link relative block py-1.5 px-3 rounded-full bg-transparent outline-none transition-all duration-300 text-gray-500 hover:text-gray-800 hover:bg-violet-100 focus:shadow-none"
+                href="#">11</a></li>
+            <li class="page-item"><a
+                class="page-link relative block py-1.5 px-3 rounded-full bg-transparent outline-none transition-all duration-300 text-gray-500 hover:text-gray-800 hover:bg-violet-100 focus:shadow-none"
+                href="#">12</a></li>
+            <li class="page-item"><a
+                class="page-link relative block py-1.5 px-3 rounded-full bg-transparent outline-none transition-all duration-300 text-gray-500 hover:text-gray-800 hover:bg-violet-100 focus:shadow-none"
+                href="#">13</a></li>
+            <li class="page-item"><a
+                class="page-link relative block py-1.5 px-3 rounded-full bg-transparent outline-none transition-all duration-300 text-gray-500 hover:text-gray-800 hover:bg-violet-100 focus:shadow-none"
+                href="#" aria-label="Next">
+                <span aria-hidden="true">&rsaquo;</span>
+              </a></li>
+          </ul>
+        </nav>
+      </div>
     </div>
-  </div>
-</body>
 </template>
 
 <script>
 
 // import TalentCard from '../components/Cards/Talents/TalentCards.vue';
-import TalentCard from '../components/Cards/Talents/TalentCards.vue'
+import TalentCard from '../components/TalentCards/TalentCards.vue'
 export default{
   components: {
     TalentCard,
