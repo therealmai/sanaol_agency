6ccf3db28c703f72c98d81787abb02915df92876
i-adriatico <58451917+i-adriatico@users.noreply.github.com>
2022-12-01 19:01:01 +0800

    feat: edit events modals cs-36

--- vue/src/pages/EditEvents.vue
+++ vue/src/pages/EditEvents.vue
@@ -1,9 +1,8 @@
 <template>
 <body style="margin-top: 20px;">
-
     
     <primary-button  text="cancel" height="25" width="100" color="White" fontSize="12" @click="$router.push('Events')"></primary-button>
-    <form style="margin-left: 320px; margin-top: 20px;">
+    <div style="margin-left: 320px; margin-top: 20px;">
         <p1 style="font-weight: bold; font-size: 30px;">Edit Events</p1><br><br><br><br>
        
         <div style="margin-left: 100px; margin-top: 30px;">
@@ -11,33 +10,35 @@
             <img  v-if="post.image" :src="post.image" style="height:184px;width:170px; position: absolute; z-index: 3" />
             <! <input type="file" id="upload-file" name="upload-file" >
             <! <label class="upload-box" for="upload-file" refs="upload-file">+</label> -->
-            <div class="file-upload-section">
-                
-                <div class="file-upload">
-                <label class="upload-box" for="file-upload" >+</label>
-                <input type="file" id="file-upload" @change="onFileChange" hidden/>
+            <form>
+                <div class="file-upload-section">
+                    
+                    <div class="file-upload">
+                    <label class="upload-box" for="file-upload" >+</label>
+                    <input type="file" id="file-upload" @change="onFileChange" hidden/>
 
-                <label id="preview" for="file-upload">
-                    <img style="height:184px;width:170px;position: absolute;margin-top: -101px; z-index: 3" :key="url" v-if="url" :src="url" />
-                </label>
+                    <label id="preview" for="file-upload">
+                        <img style="height:184px;width:170px;position: absolute;margin-top: -101px; z-index: 3" :key="url" v-if="url" :src="url" />
+                    </label>
+                    </div>
+                    <div v-if="url">
+                        <button style="position: absolute;z-index: 4;top: 10.8%;margin-left: 159px;" @click="removeImage()">
+                            <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g clip-path="url(#clip0_326_3033)">
+                                <path d="M9.43848 16.5938C13.5806 16.5938 16.9385 13.2359 16.9385 9.09375C16.9385 4.95161 13.5806 1.59375 9.43848 1.59375C5.29634 1.59375 1.93848 4.95161 1.93848 9.09375C1.93848 13.2359 5.29634 16.5938 9.43848 16.5938Z" fill="white" stroke="#525252" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path d="M11.6885 6.84375L7.18848 11.3438" stroke="#525252" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path d="M7.18848 6.84375L11.6885 11.3438" stroke="#525252" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                </g>
+                                <defs>
+                                <clipPath id="clip0_326_3033">
+                                <rect width="18" height="18" fill="white" transform="translate(0.438477 0.09375)"/>
+                                </clipPath>
+                                </defs>
+                            </svg>
+                        </button>
+                    </div>
                 </div>
-                <div v-if="url">
-                    <button style="position: absolute;z-index: 4;top: 10.8%;margin-left: 159px;" @click="removeImage()">
-                        <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <g clip-path="url(#clip0_326_3033)">
-                            <path d="M9.43848 16.5938C13.5806 16.5938 16.9385 13.2359 16.9385 9.09375C16.9385 4.95161 13.5806 1.59375 9.43848 1.59375C5.29634 1.59375 1.93848 4.95161 1.93848 9.09375C1.93848 13.2359 5.29634 16.5938 9.43848 16.5938Z" fill="white" stroke="#525252" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                            <path d="M11.6885 6.84375L7.18848 11.3438" stroke="#525252" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                            <path d="M7.18848 6.84375L11.6885 11.3438" stroke="#525252" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                            </g>
-                            <defs>
-                            <clipPath id="clip0_326_3033">
-                            <rect width="18" height="18" fill="white" transform="translate(0.438477 0.09375)"/>
-                            </clipPath>
-                            </defs>
-                        </svg>
-                    </button>
-                </div>
-            </div>
+            </form>
             
 
 <br><br><br>
@@ -50,8 +51,8 @@
             
         <div class="flex gap-4">
             <FilledButton class="mr-9" fontSize="12" color="white" text="Save" width="99" height="36" @click="showUpdateModal"/>
-            <OutlineButton class="mr-9" fontSize="12" color="white" text="Delete" width="99" height="36"/>
-            <OutlineButton fontSize="12" color="white" text="Cancel" width="99" height="36"/>
+            <OutlineButton class="mr-9" fontSize="12" color="white" text="Delete" width="99" height="36" @click="showDeleteModal"/>
+            <OutlineButton fontSize="12" color="white" text="Cancel" width="99" height="36" @click="goBackToEvents"/>
         </div><br><br><br>
         </div>
 
@@ -66,22 +67,18 @@
                 <p class="modal-paragraph">Are you sure you want to save the changes<br>made on this service? Please confirm.</p>
                 <Info text="Saving the changes will change what the public sees on the services page."></Info>
                 <div class="flex gap-10 mt-5 mb-5">
-                    <FilledButton class="w-[175px] h-[35px]" text="CONFIRM"></FilledButton>
-                    <OutlineButton class="w-[175px] h-[35px]" text="CANCEL"></OutlineButton>
+                    <FilledButton class="h-25 w-40" text="CONFIRM" @click="confirmUpdateModal"></FilledButton>
+                    <OutlineButton class="h-25 w-40" text="CANCEL" @click="closeUpdateModal"></OutlineButton>
                 </div>
             </template>
         </Modal>
         
-
          <!-- Modal appears after you click the CONFIRM button -->
-         <!-- 
-         <UpdateModal text="news article">
+         <UpdateModal text="news article" v-show="isConfirmUpdateModal">
          </UpdateModal>
-         -->
 
-         <!-- Modal appears after you click the DELETE button -->
-         <!--
-        <Modal>
+         <!-- Modal appears after you click the DELETE button -->   
+        <Modal v-show="isDeleteModalVisible">
             <template v-slot:modal_content>
             <svg xmlns="http://www.w3.org/2000/svg" width="84" height="84" viewBox="0 0 84 84" fill="none">
             <path d="M28.7109 28.7109C30.2531 27.1852 32.7469 27.1852 34.1414 28.7109L41.8523 36.4383L49.7109 28.7109C51.2531 27.1852 53.7469 27.1852 55.1414 28.7109C56.8148 30.2531 56.8148 32.7469 55.1414 34.1414L47.5617 41.8523L55.1414 49.7109C56.8148 51.2531 56.8148 53.7469 55.1414 55.1414C53.7469 56.8148 51.2531 56.8148 49.7109 55.1414L41.8523 47.5617L34.1414 55.1414C32.7469 56.8148 30.2531 56.8148 28.7109 55.1414C27.1852 53.7469 27.1852 51.2531 28.7109 49.7109L36.4383 41.8523L28.7109 34.1414C27.1852 32.7469 27.1852 30.2531 28.7109 28.7109ZM84 42C84 65.1984 65.1984 84 42 84C18.8016 84 0 65.1984 0 42C0 18.8016 18.8016 0 42 0C65.1984 0 84 18.8016 84 42ZM42 7.875C23.1492 7.875 7.875 23.1492 7.875 42C7.875 60.8508 23.1492 76.125 42 76.125C60.8508 76.125 76.125 60.8508 76.125 42C76.125 23.1492 60.8508 7.875 42 7.875Z" fill="#FF7878"/>
@@ -90,19 +87,16 @@
                 <p class="modal-paragraph">Are you sure you want to delete this<br>news article? Please confirm.</p>
                 <Warning text="Deleting this will also delete information pertaining to the news article."></Warning>
                 <div class="flex gap-10 mt-5 mb-5">
-                    <FilledButton class="w-[150px] h-[35px]" text="CONFIRM"></FilledButton>
-                    <OutlineButton class="w-[150px] h-[35px]" text="CANCEL"></OutlineButton>
+                    <FilledButton class="h-25 w-40" text="CONFIRM" @click="confirmDeleteModal"></FilledButton>
+                    <OutlineButton class="h-25 w-40" text="CANCEL" @click="closeDeleteModal"></OutlineButton>
                 </div>
             </template>
         </Modal>
-        -->
-
+        
         <!-- Modal appears after you click the CONFIRM button -->
-        <!--
-        <DeletedModal text="news article">
+        <DeletedModal text="news article" v-show="isConfirmDeleteModal">
         </DeletedModal>
-        -->
-    </form>
+    </div>
 </body>
 </template>
 
@@ -137,11 +131,28 @@ export default {
         removeImage: function () {
             this.url = null
         },
+        goBackToEvents() {
+            this.$router.push('/events');
+        },
         showUpdateModal() {
             this.isUpdateModalVisible = true;
         },
         closeUpdateModal() {
             this.isUpdateModalVisible = false;
+        },
+        confirmUpdateModal() {
+            this.isUpdateModalVisible = false;
+            this.isConfirmUpdateModal = true;
+        },
+        showDeleteModal(){
+            this.isDeleteModalVisible = true;
+        }, 
+        closeDeleteModal() {
+            this.isDeleteModalVisible = false;
+        },
+        confirmDeleteModal() {
+            this.isDeleteModalVisible = false;
+            this.isConfirmDeleteModal = true;
         }
     },
     data() {
@@ -153,6 +164,7 @@ export default {
             description: "1",
             url: null,
             isUpdateModalVisible: false,
+            isDeleteModalVisible: false,
         }
     }
  };
