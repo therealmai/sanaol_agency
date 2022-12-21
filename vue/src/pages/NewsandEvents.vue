d8b02046098c1c1edbf8436f7ebdfd6af3c03386
rlentan <18105525@usc.edu.ph>
2022-12-21 18:10:45 +0800

    Checklist Progress 1 - Tan
    Rename events.vue to newsandevents.vue

--- vue/src/pages/NewsandEvents.vue
+++ vue/src/pages/NewsandEvents.vue
@@ -2,7 +2,7 @@
 
 <body class="flex flex-col">
 
-  <div class="flex items-center justify-between pl-[15%] pr-[10%] mt-[76px] mb-[-100px] w-full">  
+  <div class="flex items-center justify-between pl-[15%] pr-[10%] mt-[76px] mb-[-100px] w-full">
     <h1 class="font-bold text-3xl mb-[20%] pb-[15%]">Events</h1>
     <div class="float-right pl-[47%] ml-[8%]  mb-[17%] w-full">
         <FilledButton text="Edit Events" class="w-[177px]"></FilledButton>
@@ -15,8 +15,8 @@
 
   <div class="flex items-center justify-between pl-[15%] pr-[10%] mb-[] w-full">
     <h1 class="font-bold text-3xl pb-[50px] w-full">News articles</h1>
-      <div class="float-right pl-[50%]  mb-[2px] w-full">
-          <FilledButton text="Add Articles" class="w-[179px] mr-[380px]"></FilledButton>
+      <div class="float-right mb-[2px] mr-[105px]">
+          <FilledButton text="Add Articles" class="w-[179px]"></FilledButton>
       </div>
 
   </div>
@@ -61,7 +61,7 @@ export default{
     FilledButton,
     OutlineButton,
     CardCarousel
-    
+
 },
 }
 </script>
