01cdecc20dcdbcc763bd6dfa7646d0384d4d3c58
ryanomongos <60454418+ryanomongos@users.noreply.github.com>
2022-12-24 15:09:24 +0800

    round style of paginatin

--- vue/src/pages/NewsandEvents.vue
+++ vue/src/pages/NewsandEvents.vue
@@ -30,9 +30,41 @@
   </div>
     <br>
     <br>
-  <div class="mt-[10px]">
-    <PaginationButtonVue class="flex flex-col items-center"></PaginationButtonVue>
-  </div>
+    <div class="flex justify-center pt-10 mb-10">
+        <nav aria-label="Page navigation">
+          <ul class="flex list-style-none">
+            <li class="page-item"><a
+                class="page-link relative block py-1.5 px-3 rounded-full bg-transparent outline-none transition-all duration-300 text-gray-500 hover:text-gray-800 hover:bg-violet-100 focus:shadow-none"
+                href="#" aria-label="Previous">
+                <span aria-hidden="true">&lsaquo;</span>
+              </a></li>
+            <li class="page-item"><a
+                class="page-link relative block py-1.5 px-3 rounded-full bg-transparent outline-none transition-all duration-300 text-gray-500 hover:text-gray-800 hover:bg-violet-100 focus:shadow-none"
+                href="#">1</a></li>
+            <li class="page-item"><a
+                class="page-link relative block py-1.5 px-3 rounded-full bg-transparent outline-none transition-all duration-300 text-gray-500 hover:text-gray-800 hover:bg-violet-100 focus:shadow-none"
+                href="#">2</a></li>
+            <li class="page-item"><a
+                class="page-link relative block py-1.5 px-3 rounded-full bg-transparent outline-none transition-all duration-300 text-gray-500 hover:text-gray-800 hover:bg-violet-100 focus:shadow-none"
+                href="#">3</a></li>
+            <span class="rounded-md px-4 py-2 text-gray-500">...</span>
+            <li class="page-item"><a
+                class="page-link relative block py-1.5 px-3 rounded-full bg-transparent outline-none transition-all duration-300 text-gray-500 hover:text-gray-800 hover:bg-violet-100 focus:shadow-none"
+                href="#">11</a></li>
+            <li class="page-item"><a
+                class="page-link relative block py-1.5 px-3 rounded-full bg-transparent outline-none transition-all duration-300 text-gray-500 hover:text-gray-800 hover:bg-violet-100 focus:shadow-none"
+                href="#">12</a></li>
+            <li class="page-item"><a
+                class="page-link relative block py-1.5 px-3 rounded-full bg-transparent outline-none transition-all duration-300 text-gray-500 hover:text-gray-800 hover:bg-violet-100 focus:shadow-none"
+                href="#">13</a></li>
+            <li class="page-item"><a
+                class="page-link relative block py-1.5 px-3 rounded-full bg-transparent outline-none transition-all duration-300 text-gray-500 hover:text-gray-800 hover:bg-violet-100 focus:shadow-none"
+                href="#" aria-label="Next">
+                <span aria-hidden="true">&rsaquo;</span>
+              </a></li>
+          </ul>
+        </nav>
+      </div>
 </body>
 
 </template>
