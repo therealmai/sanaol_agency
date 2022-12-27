ebde947276f8ea44c52365819ef6c05d0003299b
Jade Andrie Rosales <88530893+Frooge@users.noreply.github.com>
2022-12-27 21:29:28 +0800

    refactor: adjusted landing page

--- vue/src/pages/Hero.vue
+++ vue/src/pages/Hero.vue
@@ -1,42 +1,37 @@
 <template>
   <div class="flex-col">
     <!-- <div class="w-full object-cover h-screen lg:w-full md:h-screen bg-cover bg-center" style="background-image:url(https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80);"></div> -->
-    <Carousel v-if="!isLoading.hero"
-    :images="hero.images"
-    :header="getHeader"
-    :subheader="getSubheader"/>
-    <div v-else>Insert loading overlay here</div>
+    <Carousel
+    :herobanners="hero"
+    />
   <!-- Service Container -->
+  
   <div class="flex flex-row flex-wrap gap-4 justify-center ml-28 mt-16">
     <CardCarousel v-if="!isLoading.events"
     :events="events"
-    :preview="hero.preview_events"/>
-    <div v-else>Insert loading overlay here</div>
+    />
   </div>
   <Divider></Divider>
   
-  <div v-if="!isLoading.services"  class="flex flex-row flex-wrap gap-16 justify-center ml-20 mr-20 mt-14 ">
-    <ServiceCard v-for="index in 2"
-    :key="index"
-    :title="services[index].title"
-    :content="services[index].content"
-    :image="services[index].image"/>
+  <div class="flex flex-row flex-wrap gap-16 justify-center ml-20 mr-20 mt-14 ">
+    <ServiceCard v-for="service in services" v-if="!isLoading.services"
+    :key="service.id"
+    :title="service.title"
+    :content="service.content"
+    :image="service.image"/>
     <Divider></Divider>
   </div>
-  <div v-else>Insert loading overlay here</div>
 
   <div class="flex flex-col flex-wrap gap-4 items-center mx-16 mt-16">
     <Label text="NEWS AND ARTICLES" :fontSize="16" :height="29" :width="252" :padding="2" :margin="16"></Label>
-    <div  v-if="!isLoading.news" class="flex flex-row flex-wrap gap-10 justify-center">
-      <!-- for v-for it should be v-for="index in hero.preview_news" during production -->
-      <NewsCard  v-for="index in 3"
-      :key="index"
-      :title="news[index].title"
-      :content="news[index].content"
-      :image="news[index].image"
+    <div class="flex flex-row flex-wrap gap-10 justify-center">
+      <NewsCard  v-for="news in news" v-if="!isLoading.news"
+      :key="news.id"
+      :title="news.title"
+      :content="news.content"
+      :image="news.image"
       hover="lightgray"/>
     </div>
-    <div v-else>Insert loading overlay here</div>
     <Divider></Divider>
   </div>
   
@@ -55,7 +50,7 @@ import Label from '../components/Label/Label.vue';
 import Carousel from '../components/HeroBanner/Carousel.vue';
 import CardCarousel from '../components/HeroBanner/CardCarousel.vue';
 import axios from '../axios';
-import store from '../store';
+
 export default{
   components: {
     FilledButton,
@@ -70,7 +65,7 @@ export default{
   },
   data() {
     return {
-      hero: {},
+      hero: [],
       services: [],
       events: [],
       news: [],
@@ -84,14 +79,10 @@ export default{
   },
   methods: {
     getHeroBanner() {
-      const params = 1; // sample params for herobanner
-      axios.get('herobanner/'+params)
+      axios.get('herobanner')
       .then((res) => {
-        this.hero = res.data.data;
-        return axios.get('herobanner/image/'+this.hero.id)
-      })
-      .then((res) => {
-        this.hero['images'] = res.data.data;
+        this.hero = res.data.slice(0,3); // only 3 herobanners to display
+        console.log(this.hero);
         this.isLoading.hero = false;
       })
       .catch((err) => {
@@ -101,7 +92,7 @@ export default{
     getServices() {
       axios.get('services')
       .then((res) => {
-        this.services = res.data;
+        this.services = res.data.slice(0,2); // only 2 services to display
         this.isLoading.services = false;
       })
       .catch((err) => {
@@ -109,9 +100,17 @@ export default{
       })
     },
     getEvents() {
-      axios.get('events')
+      axios.get('preview/events')
       .then((res) => {
-        this.events = res.data;
+        return Promise.all(res.data.slice(0,3).map(element => { // only 3 events to display
+            axios.get('events/'+element.events_id)
+            .then((res) => {
+              this.events.push(res.data.data);
+            })
+          })
+        )
+      })
+      .then(() => {
         this.isLoading.events = false;
       })
       .catch((err) => {
@@ -119,9 +118,17 @@ export default{
       })
     },
     getNews() {
-      axios.get('news')
+      axios.get('preview/news')
       .then((res) => {
-        this.news = res.data;
+        return Promise.all(res.data.slice(0,3).map(element => { // only 3 news to display
+            axios.get('news/'+element.news_id)
+            .then((res) => {
+              this.news.push(res.data.data);
+            })
+          })
+        )
+      })
+      .then(() => {
         this.isLoading.news = false;
       })
       .catch((err) => {
@@ -129,14 +136,6 @@ export default{
       })
     },
 
-  },
-  computed: {
-    getHeader() {
-      return store.state.user !== null ? this.hero.header_gen : this.hero.header_tal;
-    },
-    getSubheader() {
-      return store.state.user !== null ? this.hero.subheader_gen : this.hero.subheader_tal;
-    }
   },
   mounted() {
     this.getHeroBanner();
