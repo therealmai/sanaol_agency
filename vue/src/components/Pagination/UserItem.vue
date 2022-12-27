c165f2b181b29179a54e2204cfad19929af63a30
w3barsi <darcybalaga@gmail.com>
2022-12-27 23:55:16 +0800

    added usermgmt edit and delete

--- vue/src/components/Pagination/UserItem.vue
+++ vue/src/components/Pagination/UserItem.vue
@@ -1,51 +1,103 @@
 <template>
-    <div class="flex min-w-max justify-between gap-x-2 py-1 px-8 bg-white border-t border-divider text-[#696969]">
-      <div class="w-3/12 min-w-max grid grid-cols-4 grid-rows-4">
-        <div class=" text-base col-span-1 row-span-4"><img width="34px" height="34px" :src="data.image"/></div>
-        <div class=" text-base col-span-3 row-span-2">{{fname}} {{lname}}</div>
-        <div class=" text-base col-span-3 row-span-2">{{handle}}</div>
+  <div
+    class="flex min-w-max justify-between gap-x-2 py-1 px-8 bg-white border-t border-divider text-[#696969]"
+  >
+    <div class="w-3/12 min-w-max grid grid-cols-4 grid-rows-4">
+      <div class="text-base col-span-1 row-span-4">
+        <img width="34px" height="34px" :src="data.image" />
       </div>
-      <span class="w-4/12 min-w-max text-base ">{{email}}</span>
-      <span class="w-2/12 min-w-max text-base ">{{user_type}}</span>
-      <span class="flex flex-row justify-center flex-nowrap text-[#696969] gap-x-2">
-        <FilledButton class="w-[100px]" text="EDIT" />
-        <OutlineButton class="w-[100px]" text="DELETE" />
-      </span>
+      <div class="text-base col-span-3 row-span-2">{{ fname }} {{ lname }}</div>
+      <div class="text-base col-span-3 row-span-2">{{ handle }}</div>
     </div>
-  </template>
-  
-  <script>
-  import axiosClient from "../../axios";
-  import FilledButton from '../Buttons/FilledButton.vue';
-  import OutlineButton from '../Buttons/OutlineButton.vue';
-  import EditUserModal from "../Modal/UserManagementModals/UserManagementModals/EditUserModal.vue";
+    <span class="w-4/12 min-w-max text-base">{{ email }}</span>
+    <span class="w-2/12 min-w-max text-base">{{ user_type }}</span>
+    <span
+      class="flex flex-row justify-center flex-nowrap text-[#696969] gap-x-2"
+    >
+      <FilledButton
+        class="w-[100px]"
+        text="EDIT"
+        @click="() => (this.isEditModalVisible = true)"
+      />
+      <OutlineButton
+        class="w-[100px]"
+        text="DELETE"
+        @click="() => (this.isDeleteModalVisible = true)"
+      />
+    </span>
+  </div>
+    <EditUserModal v-show="isEditModalVisible"/>
+  <DeleteUserConfirmationModal
+    :name="fname"
+    v-show="isDeleteModalVisible"
+    @close="closeDeleteModal"
+    @confirm="deleteUser"
+  />
+</template>
 
-  export default {
-    name: "UserItem",
-    components: { FilledButton, OutlineButton, EditUserModal },
-    data() {
-      return {
-        data:[],
-      }
+<script>
+import axios from "../../axios";
+import FilledButton from "../Buttons/FilledButton.vue";
+import OutlineButton from "../Buttons/OutlineButton.vue";
+import EditUserModal from "../Modal/UserManagementModals/UserManagementModals/EditUserModal.vue";
+import DeleteUserConfirmationModal from "../Modal/UserManageModals/DeleteUserConfirmationModal.vue";
+
+export default {
+  name: "UserItem",
+  components: {
+    FilledButton,
+    OutlineButton,
+    DeleteUserConfirmationModal,
+    EditUserModal
+},
+  data() {
+    return {
+      isDeleteModalVisible: false,
+      isEditModalVisible: false,
+      image: String,
+      data: String,
+    };
+  },
+  props: {
+    id: Number,
+    fname: String,
+    lname: String,
+    handle: String,
+    email: String,
+    user_type: String,
+  },
+  methods: {
+    loadImages() {
+      axios.get("/user/image/" + this.id).then((res) => {
+        if (res.status == 200) {
+          this.data.image = res.data?.image;
+        } else {
+          console.log("Failed to get images");
+        }
+      });
     },
-    props: {
-      id: String,
-      fname: String,
-      lname: String,
-      handle: String,
-      email: String,
-      user_type: String
+    showDeleteModal() {
+      this.isDeleteModalVisible = true;
     },
-    methods: {
-      loadImages(){
-          axiosClient.get("/user/image/"+this.id).then(({ data }) => (this.data = data.data[0]));
-      },
+    closeDeleteModal() {
+      console.log("BEFORE", this.isDeleteModalVisible);
+      this.isDeleteModalVisible = false;
+      console.log("AFTER", this.isDeleteModalVisible);
+    },
+    deleteUser() {
+      console.log("Testing emit");
+      axios.patch(`users/delete/${this.id}`).then((res) => {
+        if (res.status == 200) {
+          this.isDeleteModalVisible = false;
+          console.log(res);
+        }
+      });
+    },
   },
   mounted() {
     this.loadImages();
-  }
-  };
-  </script>
-  
-  <style lang="postcss" scoped>
-  </style>
+  },
+};
+</script>
+
+<style lang="postcss" scoped></style>
