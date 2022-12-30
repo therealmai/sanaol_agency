0d70f18c4743fa8ce4ac5506fac25fb5521c2c31
w3barsi <darcybalaga@gmail.com>
2022-12-31 04:46:19 +0800

    added updating is_member to true to "approve"

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -154,6 +154,7 @@ class UserController extends Controller
 
         //approve user
         $user->user_type = 'talent';
+        $user->is_member = true;
 
         if($user){
             $msg = $this->MSG_SUC_UPDATE;
